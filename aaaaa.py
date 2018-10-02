3b1c1335863ed8003a150e1adc4636bbe47b20d1
lankevamsikiran <vamsikiran1303@gmail.com>
2018-10-02 16:07:17 +0530

    adding python file

---
+++ aaaaa.py
@@ -0,0 +1,13 @@
+import tkinter
+top = tkinter.Tk()
+# Code to add widgets will go here...
+top.mainloop()
+top = tkinter.Tk()
+# Code to add widgets will go here...
+top.mainloop()
+top = tkinter.Tk()
+# Code to add widgets will go here...
+top.mainloop()
+top = tkinter.Tk()
+# Code to add widgets will go here...
+top.mainloop()

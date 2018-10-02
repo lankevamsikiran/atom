f9baf5c460941588a9460e43d303f50617255239
lankevamsikiran <vamsikiran1303@gmail.com>
2018-10-02 16:17:21 +0530

    test 3 adding

---
+++ calc.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+# if you are still working under a Python 2 version,
+# comment out the previous line and uncomment the following line
+# import Tkinter as tk
+
+root = tk.Tk()
+
+w = tk.Label(root, text="Hello Tkinter!")
+w.pack()
+
+root.mainloop()
+print("git hub")
+print("vamsi")
+print("kiran")
